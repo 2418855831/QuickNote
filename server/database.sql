a0f449833f75b213200db5518657dfecec9c6f33
qjx <2418855831@qq.com>
2020-04-22 15:13:53 +0800

    database.sql

--- server/database.sql
+++ server/database.sql
@@ -1,5 +1,3 @@
-CREATE DATABASE  IF NOT EXISTS `quick_note` /*!40100 DEFAULT CHARACTER SET utf8 */;
-USE `quick_note`;
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
 -- Host: 127.0.0.1    Database: quick_note
@@ -364,4 +362,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2020-04-22 13:53:12
+-- Dump completed on 2020-04-22 15:13:27
